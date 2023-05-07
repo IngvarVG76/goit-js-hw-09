781fc69de97359e42a62001845759b9df68d0e63
IngvarVG76 <106869716+IngvarVG76@users.noreply.github.com>
2023-05-07 22:37:44 +0000

    Deploying to gh-pages from @ IngvarVG76/goit-js-hw-09@6c53f89d7b79608362f8c135288f2111d50acf76 🚀

---
+++ 01-color-switcher.f568729b.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("body"),a=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0,a.addEventListener("click",(function(){a.disabled=!0,d.disabled=!1,t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));e.style.backgroundColor=t}),1e3)})),d.addEventListener("click",(function(){clearInterval(t),a.disabled=!1,d.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.f568729b.js.map
